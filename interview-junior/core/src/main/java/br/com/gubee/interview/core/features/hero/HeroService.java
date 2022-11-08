4ac1a4df8d7172ba7602df568ece78403c61af65
JoseAP0 <j.augustoifg@gmail.com>
2022-11-08 14:25:36 -0300

    FIX: Hero and PowerStats savings

--- interview-junior/core/src/main/java/br/com/gubee/interview/core/features/hero/HeroService.java
+++ interview-junior/core/src/main/java/br/com/gubee/interview/core/features/hero/HeroService.java
@@ -1,6 +1,8 @@
 package br.com.gubee.interview.core.features.hero;
 
+import br.com.gubee.interview.core.features.powerstats.PowerStatsService;
 import br.com.gubee.interview.model.Hero;
+import br.com.gubee.interview.model.PowerStats;
 import br.com.gubee.interview.model.request.CreateHeroRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,12 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class HeroService {
 
+    private final PowerStatsService powerStatsService;
     private final HeroRepository heroRepository;
+
 
     @Transactional
     public UUID create(CreateHeroRequest createHeroRequest) {
-        return heroRepository.create(new Hero(createHeroRequest, null));
+        return heroRepository.create(new Hero(createHeroRequest, powerStatsService.create(new PowerStats(createHeroRequest))));
     }
 }
